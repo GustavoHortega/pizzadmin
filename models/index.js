e2f0691d13004dccafb43e4c2d99e3703e5ff467
Gustavo Hortega <gustavohortega80@gmail.com>
2024-11-26 16:40:40 -0400

    Faça a exportação do schema do cliente.

--- models/index.js
+++ models/index.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
+const clienteSchema = require('./cliente');
+
+const Cliente = mongoose.model('Cliente', clienteSchema);
+
 const connect = () =>{
+
+
     mongoose.connect('mongodb://localhost:27017/pizzadmin');
 }
 
-module.exports={
+module.exports = {
     connect,
+    Cliente,
 }
